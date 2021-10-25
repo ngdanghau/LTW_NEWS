ce3a724c5896cfab4ed775c33764f95e346ee009
ngdanghau <ngdghau201@gmail.com>
2021-10-25 14:42:02 +0700

    Thêm bài viết gần đây ở index.jsp

--- src/client/controllers/IndexController.java
+++ src/client/controllers/IndexController.java
@@ -3,14 +3,17 @@ package client.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import entities.Posts;
@@ -53,19 +56,47 @@ public class IndexController {
 		return query.list();
 	}
 	
-	@RequestMapping("index")
-	public String index(ModelMap model){
-		List<WidgetModel> lists = new ArrayList<WidgetModel>();
-		List<Widgets> listWidgets = this.getListWidgets();
-		for(Widgets item: listWidgets) {
-			lists.add(
-	    			new WidgetModel(
-	    					item.getCategory(), 
-	    					this.getListPostsByCat(item.getCategory().getId(), item.getNum_post()), 
-	    					item.getLayout().trim()
-	    			)
-	    	);
+	@SuppressWarnings("unchecked")
+	public List<Posts> getPostRecent(){
+		Session session = factory.getCurrentSession();
+		String hql = "SELECT p FROM Posts p"; 
+		Query query = session.createQuery(hql); 
+		try 
+		{
+			return query.list();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return null;
 		}
+		
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping("index")
+	public String index(HttpServletRequest request,ModelMap model){
+		List<WidgetModel> lists = new ArrayList<WidgetModel>();
+		
+		List<Posts> posts = getPostRecent();
+		PagedListHolder pagedlistHolder = new PagedListHolder(posts);
+		int page = ServletRequestUtils.getIntParameter(request, "p", 0);
+		pagedlistHolder.setPage(page);
+		pagedlistHolder.setMaxLinkedPages(5);
+		pagedlistHolder.setPageSize(9);
+		
+		if(page == 0) {
+			List<Widgets> listWidgets = this.getListWidgets();
+			for(Widgets item: listWidgets) {
+				lists.add(
+		    			new WidgetModel(
+		    					item.getCategory(), 
+		    					this.getListPostsByCat(item.getCategory().getId(), item.getNum_post()), 
+		    					item.getLayout().trim()
+		    			)
+		    	);
+			}
+		}
+		
+		model.addAttribute("pagedListHolder", pagedlistHolder);
 		model.addAttribute("listWidgets", lists);
 		return "index";
 	}
