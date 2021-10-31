fbaf5ee6900406f65abad3182832be75ce6dbe41
ngdanghau <ngdghau201@gmail.com>
2021-10-31 19:26:01 +0700

    xóa thư viện không dùng

--- src/admin/controllers/SettingsController.java
+++ src/admin/controllers/SettingsController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import entities.General_Data;
