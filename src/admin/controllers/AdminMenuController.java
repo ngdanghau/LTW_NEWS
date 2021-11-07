f6f75d3baf0c43ca6fa55c3571b39b5ac1a05b12
Dang Hau Nguyen <33993567+ngdanghau@users.noreply.github.com>
2021-11-07 07:35:24 +0700

    fix warning

--- src/admin/controllers/AdminMenuController.java
+++ src/admin/controllers/AdminMenuController.java
@@ -10,10 +10,8 @@ import org.hibernate.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,8 +19,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import entities.Menu;
 import models.MenuModel;
 import models.MenuOrder;
-import models.MenuOrderWrapper;
-import models.SettingsData;
 
 @Transactional
 @Controller
